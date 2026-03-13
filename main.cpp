2e6612a90e5d1fb579ced2fdbf5efefa88c53df4
Hervey67 <herv2007@gmail.com>
2026-03-13 16:08:58 -0700

    Changes to main.cpp

--- main.cpp
+++ main.cpp
@@ -6,16 +6,31 @@ using namespace std;
 int main()
 {
     double a, b, c;
+    double x1, x2;
     // TODO
+    cin >> a >> b >> c;
 
     // TODO
-    // TODO
-    // TODO
+
+    x1 = (-b + sqrt((b * b) - (4 * a * c))) / (2 * a);
 
     // TODO
+
+    x2 = (-b - sqrt((b * b) - (4 * a * c))) / (2 * a);
+
     // TODO
 
     cout << setprecision(2) << fixed;
+    
+    if (x1 > x2){
+        cout << x1 << " " << x2;
+    }
+    else{ 
+        cout << x2 << " " << x1;
+    }
+    // TODO
+    // TODO
+
     // TODO
 
     return 0;
